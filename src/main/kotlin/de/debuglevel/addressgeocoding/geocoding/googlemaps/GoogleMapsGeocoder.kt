70490a2d5551256efbb4faa058a477514bef88e7
Marc Kohaupt <debuglevel@gmail.com>
2021-03-16 22:31:08 +0100

    Add GoogleMapsGeocoder stub

---
+++ src/main/kotlin/de/debuglevel/addressgeocoding/geocoding/googlemaps/GoogleMapsGeocoder.kt
@@ -0,0 +1,10 @@
+package de.debuglevel.addressgeocoding.geocoding.googlemaps
+
+import de.debuglevel.addressgeocoding.geocoding.Coordinate
+import de.debuglevel.addressgeocoding.geocoding.Geocoder
+
+class GoogleMapsGeocoder : Geocoder {
+    override fun getCoordinates(address: String): Coordinate {
+        TODO("Not yet implemented")
+    }
+}
