daa8e35161c99ac249fa8d3e704aa1da32f2e1ed
Marc Kohaupt <debuglevel@gmail.com>
2021-03-17 19:46:50 +0100

    Return longitude and latitude on POST request Fixes #7

--- src/main/kotlin/de/debuglevel/addressgeocoding/geocode/AddGeocodeResponse.kt
+++ src/main/kotlin/de/debuglevel/addressgeocoding/geocode/AddGeocodeResponse.kt
@@ -5,9 +5,13 @@ import java.util.*
 data class AddGeocodeResponse(
     val id: UUID,
     val address: String,
+    val longitude: Double?,
+    val latitude: Double?,
 ) {
     constructor(geocode: Geocode) : this(
         geocode.id!!,
         geocode.address,
+        geocode.longitude,
+        geocode.latitude,
     )
 }
